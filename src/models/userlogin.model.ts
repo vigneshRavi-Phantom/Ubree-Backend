eed4bbb63d693fd7fc11550f4d383bb3e5f0c714
VarthanV <vishnulatha006@gmail.com>
2020-09-19 17:05:04 +0530

    Completed Login Model

---
+++ src/models/userlogin.model.ts
@@ -0,0 +1,55 @@
+import { Sequelize } from "sequelize";
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../dbconfig";
+export class UserLogin extends Model {
+  public id!: number;
+  public mobileNumber!: string;
+  public otp!: number;
+  public userType!: number;
+  public isActive!: boolean;
+}
+UserLogin.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    mobileNumber: {
+      type: new DataTypes.STRING(10),
+      allowNull: false,
+    },
+    otp: {
+      type: new DataTypes.INTEGER(),
+      allowNull: true,
+    },
+    userType: {
+      type: new DataTypes.INTEGER(),
+      allowNull: false,
+    },
+    isActive: {
+      type: new DataTypes.INTEGER(),
+      defaultValue: 0,
+    },
+    created: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+    createdBy: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    modified: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+    modifiedBy: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
+    sequelize,
+  }
+);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express ,{Request,Response,NextFunction} from 'express';
 import {generateOTP} from '../jobs/generateOtp';
-import {User} from '../models/user.model';
+import {User} from '../models/userlogin.model';
 const app = express.Router();       
 
 app.get('/', async (req:Request,res:Response,next:NextFunction) =>{

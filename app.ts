22dcbaa0f39f80e4d269ae84aa2b21f532672e1f
VarthanV <vishnulatha006@gmail.com>
2020-09-20 00:26:33 +0530

    Added boiler plate for Swagger ui

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express ,{Application,Request,Response,NextFunction} from 'express';
-import { sequelize } from './dbconfig';
-import { authRoute } from './routes/auth';
+import { sequelize } from './src/dbconfig';
+import { authRoute } from './src/routes/auth';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
+
 const app:Application= express();
 const syncDb =async()=>{
 await sequelize.sync({force:true});
@@ -10,7 +13,7 @@ console.log("All models were synchronized successfully.");
 syncDb();
 app.use(bodyParser.json());
 app.use('/auth',authRoute);
-
+app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 app.listen(3000,()=>{
     console.log("Listening on Port 3000");
 })
